77cc19db97606b36ab83c31941203082c7bba63a
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2015-02-03 13:53:40 -0800

    basic signin works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,25 @@
 class User < ActiveRecord::Base
   validates :session_token, presence: true
   
-  after_initialize :ensure_session_token
-  
-  
-  private
-  
-  def ensure_session_token
-    self.session_token ||= SecureRandom.urlsafe_base64(16)
-  end
+  after_initialize :ensure_session_token, :set_game_stats
   
   def reset_token!
     self.session_token = SecureRandom.urlsafe_base64(16)
     self.save!
     self.session_token
+  end 
+  
+  
+  private
+  
+  
+  def set_game_stats
+    self.games_won = 0
+    self.games_lost = 0
   end
   
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
   
 end
